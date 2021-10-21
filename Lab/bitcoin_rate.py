bd23fcf9af7350cee4c1a8ec4e40ed70784c960e
Jake-Fischer <jakefischer000@gmail.com>
2021-10-20 21:04:49 -0500

    Created a unit test

--- Lab/bitcoin_rate.py
+++ Lab/bitcoin_rate.py
@@ -2,33 +2,29 @@ import requests
 
 
 def main():
-    # json_data = get_response()
-    # exchange_rate = get_exchange_rate(json_data)
-    exchange_rate = get_btc_to_usd()
+    exchange_rate = get_bitcoin_exchange_rate()
     user_bitcoin = get_user_bitcoin()
     bitcoin_value_in_usd = calculate_value_in_USD(user_bitcoin, exchange_rate)
     display_results(user_bitcoin, bitcoin_value_in_usd)
 
 
-def get_btc_to_usd():
-    data = get_response()
-    rate = get_exchange_rate(data)
-    return rate
-
-def get_response():
+def get_bitcoin_exchange_rate():
+    """Gets the exchange rate and returns it"""
     coindesk_url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
     response = requests.get(coindesk_url)
-    return response.json()
-
-
-def get_exchange_rate(json_data):
-    dollars_exchange_rate = json_data['bpi']['USD']['rate_float']
+    data = response.json()
+    dollars_exchange_rate = data['bpi']['USD']['rate_float']
     return dollars_exchange_rate
 
 
 def get_user_bitcoin():
-    bitcoin = float(input('Enter number of bitcoins: '))
-    return bitcoin
+    """Asks the user for a number greater than 0 and then returns it"""
+    while True:
+        bitcoin = float(input('Enter number of bitcoins: '))
+        if bitcoin > 0:
+            return bitcoin
+        else:
+            print('Please enter an amount greater than 0')
 
 
 def calculate_value_in_USD(bitcoin, exchange_rate):
--- Lab/test_bitcoin_rate.py
+++ Lab/test_bitcoin_rate.py
@@ -4,23 +4,26 @@ from unittest.mock import patch
 
 import bitcoin_rate
 
+"""
+This is confusing stuff for me, I did my best. I tried to structure it similar to test_exchange_rate.py because it seemed similar. 
+I am unsure why the program runs and prompts the user for data when the tests are run. But it does do that.
+"""
+
 class TestBitcoinRate(TestCase):
 
-    @patch('bitcoin_rate.get_response')
-    def test_get_btc_to_usd(self, mock_btc_api):
+    @patch('bitcoin_rate.get_bitcoin_exchange_rate')
+    def test_get_bitcoin_exchange_rate(self, mock_rates):
 
-        mock_btc_api.return_value = {"time":
+        mock_rate = 5
+        example_api_response = {"time":
         {"updated":"Oct 21, 2021 00:08:00 UTC","updatedISO":"2021-10-21T00:08:00+00:00","updateduk":"Oct 21, 2021 at 01:08 BST"},
         "disclaimer":"This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org",
         "chartName":"Bitcoin",
-        "bpi":{"USD":{"code":"USD","symbol":"&#36;","rate":"65,830.9050","description":"United States Dollar","rate_float":123.0},
+        "bpi":{"USD":{"code":"USD","symbol":"&#36;","rate":"65,830.9050","description":"United States Dollar","rate_float":mock_rate},
         "GBP":{"code":"GBP","symbol":"&pound;","rate":"47,627.5406","description":"British Pound Sterling","rate_float":47627.5406},
         "EUR":{"code":"EUR","symbol":"&euro;","rate":"56,495.0952","description":"Euro","rate_float":56495.0952}}}
-
-        expected_dollars = 1230.0
-        # mock_response.side_effect = [example_api_response]
-
-        actual_dollars = bitcoin_rate.get_btc_to_usd(10)
-        expected = 1230
-
-        self.assertEqual(expected, converted)
+        
+        mock_rates.side_effect = [ example_api_response ]
+        dollar_amount = bitcoin_rate.calculate_value_in_USD(10, 5) # I think this is correct, but it seems strange that I am just feeding it 5 instead of having it parse the data.
+        expected_dollars = 50
+        self.assertEqual(expected_dollars, dollar_amount)
